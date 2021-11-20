6f6a3f1ba147d606047549271af83fc333ae286b
joshuasia <joshsia@student.ubc.ca>
2021-11-19 16:56:43 -0800

    Add test functions

--- src/get_kaggle_data.R
+++ src/get_kaggle_data.R
@@ -5,22 +5,24 @@
 Usage: get_data.R ((--url=<url> --file=<file>) | --path=<path>) [--save=save]
 
 Options:
---url=<url>       Takes a URL as a string (either this or the path is a
+--url=<url>       Takes a URL (either this or the path is a
                   required option)
---file=<file>     Takes a filename as a string (file must be specified
+--file=<file>     Takes a filename (file must be specified
                   if using URL)
 --path=<path>     Takes a local path as a string
 --save=save       Takes a local path to save the output file to as a string  
                   [default: data/rawdata.csv]
 " -> doc
 
-# --url="https://www.kaggle.com/johnjdavisiv/us-counties-covid19-weather-sociohealth-data"
-# --file="us_county_sociohealth_data.csv"
+# --url=https://www.kaggle.com/johnjdavisiv/us-counties-covid19-weather-sociohealth-data
+# --file=us_county_sociohealth_data.csv
 
 library(httr)
 library(jsonlite)
 library(docopt)
 library(tidyverse)
+library(testthat)
+
 opt <- docopt(doc)
 
 main <- function(opt) {
@@ -29,24 +31,48 @@ main <- function(opt) {
     df <- read_csv(opt$path)
   }
   else {
-    idx <- str_locate(opt$url, ".com")
-    ref <- str_sub(opt$url, start = idx[2] + 2, end = -1)
     user <- fromJSON("kaggle.json", flatten = TRUE)
     .kaggle_base_url <- "https://www.kaggle.com/api/v1"
     
-    url <- paste0(.kaggle_base_url, "/datasets/download/", ref, "/", opt$file)
+    url <- get_url(opt$url, .kaggle_base_url, opt$file)
     
-    rcall <- httr::GET(url, httr::authenticate(user$username, user$key, type="basic"))
+    rcall <- httr::GET(
+      url,
+      httr::authenticate(user$username, user$key, type="basic")
+      )
     content_type <- rcall[[3]]$`content-type`
-    
+
     if (grepl("zip", content_type)) {
       temp <- tempfile()
       download.file(rcall$url, temp)
       df <- read.csv(unz(temp, opt$file))
       unlink(temp)
     }
+    else {
+      stop("URL does not lead to a valid data set.")
+    }
   }
   write.csv(df, opt$save, row.names = FALSE)
 }
+
+get_url <- function(url, .kaggle_base_url, filename) {
+  idx <- str_locate(url, ".com")
+  ref <- str_sub(url, start = idx[2] + 2, end = -1)
+  paste0(.kaggle_base_url, "/datasets/download/", ref, "/", filename)
+}
+
+test_get_url <- function() {
+  test_that("URL is parsed incorrectly", {
+    expect_equal(
+      get_url("kaggle.com/user/data", "https://www.kaggle.com/api/v1", "file"),
+      "https://www.kaggle.com/api/v1/datasets/download/user/data/file"
+      )
+  })
+  test_that("Output object should be a string", {
+    expect_type(get_url("a", "b", "c"), "character")
+  })
+}
+
+test_get_url()
 
 main(opt)
