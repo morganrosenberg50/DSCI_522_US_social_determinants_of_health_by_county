f768de687b6855c9c340e9ed86fe892638798afb
Morgan Rosenberg <morgan.ar.rosenberg@gmail.com>
2021-11-25 15:37:44 -0800

    Adding finalized version with defensive programming and edits based on in person discussion today

--- src/data_wrangling.R
+++ src/data_wrangling.R
@@ -1,13 +1,13 @@
-#author: Morgan Rosenberg
+#author: Group 25
 # date: 2021-11-24
+
 "Cleans and organizes data from US Counties: COVID19 + Weather + Socio/Health data (https://www.kaggle.com/johnjdavisiv/us-counties-covid19-weather-sociohealth-data)
 Writes the data to a separate feather file.
 
-Usage:
+Usage: src/data_wrangling.r --input=<input> --output=<output>
 
 Options:
 --input=<input>       Path to the raw data, including the filename (e.g. data/file.csv)
---col_list=<col_list>  List of columns which may be analyzed
 --output=<output>     Path NOT including new filename for cleaned data (e.g. data/production)
 " -> doc
 
@@ -18,26 +18,39 @@ set.seed(2021)
 
 opt <- docopt(doc)
 
-main <- function(input, output){
+main <- function(inpath, output){
   #read the data
-  inpath = here(strsplit(input, "/"))
-  outpath = here(strsplit(output, "/"), "cleaned_data.csv")
-  if (!file.exists(path))
-    stop("Invalid path")
+  outpath <- here(output, "cleaned_data.csv")
+  if (!file.exists(inpath)){
+    stop("Input file not found")
+  }
+  if (!dir.exists(output)){
+    stop("No output directory found")
+  }
   
-  raw_data <- read_csv(path)
-  filtered_data <- filtere_data(raw_data)
+  raw_data <- read_csv(inpath)
+  
+  if (typeof(raw_data) != typeof(data.frame())){
+    stop("Data type is not a dataframe")
+  }
+  filtered_data <- filter_data(raw_data)
+  write_csv(filtered_data, outpath)
 }
 
 filter_data <- function(raw_data){
+  col_list = c(
+    "date", "county", "cases", "state", 
+    "total_population", "num_deaths", "percent_smokers", 
+    "percent_vaccinated", "income_ratio", 
+    "population_density_per_sqmi", "percent_fair_or_poor_health",
+    "percent_unemployed_CHR", "violent_crime_rate", 
+    "chlamydia_rate", "teen_birth_rate"
+  )
   filtered_data <- raw_data |>
     select(all_of(col_list)) |>
     drop_na() |> 
-    group_by(state) |>
-    mutate(
-      cases_growth_rate = (cases - lag(cases)) / lag(cases))|> 
-    ungroup() |> 
     group_by(county) |>
+    mutate(cases_growth_rate= c(0, diff(cases)) / cases)|>
     summarize(
       state = first(state),
       max_cases = max(cases),
@@ -52,8 +65,4 @@ filter_data <- function(raw_data){
   ## important note: teen birth rate is per thousand females, all other rates are per 100
 }
 
-  try({
-    dir.create(output)
-  })
-  saveRDS()
-}
+main(opt$input, opt$output)
